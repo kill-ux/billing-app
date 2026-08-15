92030d9bded5c27a0eba5fb3f73ee9203b699649
kill-ux <mustaphaboutoubdev@gmail.com>
2026-08-15 15:54:38 +0100

    fix: add unit tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,7 +2,7 @@ import pytest, os, requests
 
 @pytest.fixture(scope="session")
 def app_url():
-    return os.getenv("APP_URL", "http://inventory-app:8000")
+    return os.getenv("APP_URL", "http://billing-app:8000")
 
 @pytest.fixture(scope="session")
 def client(app_url):
